6f5cf0e083893efc4b99f42359b9709e7a594143
teflonpog <pog@tuta.io>
2016-03-07 05:11:14 +0000

    complete functions.js

---
+++ projects/more-matchy/part1.js
@@ -0,0 +1,46 @@
+//Use this file to implement Part One of your project
+
+// function search(name) {
+//     for (var i = 0; i < animals.length; i++) {
+//         if (name.toLowerCase() === animals[i].name.toLowerCase()) {
+//             return animals[i];
+//         }
+//     } 
+//     return null;
+// }
+
+// // Step 2 //
+
+// function edit(name, object) {
+//     for (var i = 0; i < animals.length; i++) {
+//         if (name === animals[i].name) {
+//             animals[i] = object;
+//             return animals[i];
+//         }
+//     }
+// }
+
+// // Step 3 //
+
+// function remove(name) {
+//     for (var i = 0; i < animals.length; i++) {
+//         if (name === animals[i].name) {
+//             animals.splice(i, 1);
+//         }
+//     }
+// }
+
+// // Step 4 //
+
+// function create(object) {
+//     if (object.name.length > 0 && object.species.length > 0) {
+//         for (var i = 0; i < animals.length; i++) {
+//             if (object.name === animals[i].name) {
+//                 return;
+//             }
+//         }
+//     }
+//     animals.push(object);
+// }
+
+var animal;
